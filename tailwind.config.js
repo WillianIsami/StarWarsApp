e5dc2f5ee180e74e71dccd1371c463cfa8970048
WillianIsami <willianisami1229@gmail.com>
2026-02-28 21:01:15 -0300

    build(deps): align Tailwind/PostCSS with Angular 18

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        saber: {
+          gold: '#fdd056',
+          blue: '#73d6ff'
+        }
+      }
+    }
+  },
+  plugins: []
+};
